4bf385b03641e757bcff5ea6979e55bc880ec673
Sascha Künstler <sascha.kuenstler@moccu.com>
2019-02-15 13:47:54 +0100

    Test, bitch!

--- src/js/bitchify.test.js
+++ src/js/bitchify.test.js
@@ -11,4 +11,36 @@ describe('Bitchify', () => {
 		expect(bitchify.active).toBeTruthy();
 	});
 
+	it('should override options, bitch!', () => {
+		const
+			options = {
+				elements: 'h1',
+				pattern: 'foo',
+				replace: 'bar'
+			},
+			bitchify = new Bitchify(options).render()
+		;
+
+		expect(bitchify.options.elements).toBe(options.elements);
+		expect(bitchify.options.pattern).toBe(options.pattern);
+		expect(bitchify.options.replace).toBe(options.replace);
+	});
+
+	it('should return itself on render, bitch!', () => {
+		const bitchify = new Bitchify();
+
+		expect(bitchify.render()).toBe(bitchify);
+	});
+
+	it('should render only once, bitch!', () => {
+		const bitchify = new Bitchify({
+			active: true
+		});
+
+		expect(bitchify.active).toBeTruthy();
+
+		// TODO: return itself?
+		expect(bitchify.render()).toBeFalsy();
+	});
+
 });
