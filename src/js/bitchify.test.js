4a0906a3c60395c3886ac505a437b812789422ad
Sascha Künstler <sascha.kuenstler@moccu.com>
2019-02-18 11:55:28 +0100

    To test or not to test

--- src/js/bitchify.test.js
+++ src/js/bitchify.test.js
@@ -1,14 +1,32 @@
 import Bitchify from './bitchify';
 
 
+function __fixture() {
+	return `
+		<h1>Lorem ipsum dolor</h1>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+	`;
+}
+
+
 describe('Bitchify', () => {
 
+	afterEach(() => {
+		document.body.innerHTML = '';
+	});
+
 	it('should render, bitch!', () => {
-		const bitchify = new Bitchify();
+		document.body.innerHTML = __fixture();
+
+		const
+			bitchify = new Bitchify(),
+			re = new RegExp(bitchify.options.replace, 'g')
+		;
 
 		expect(bitchify.active).toBeFalsy();
 		bitchify.render();
 		expect(bitchify.active).toBeTruthy();
+		expect((document.body.innerHTML.match(re) || []).length).toBe(2);
 	});
 
 	it('should override options, bitch!', () => {
@@ -33,14 +51,28 @@ describe('Bitchify', () => {
 	});
 
 	it('should render only once, bitch!', () => {
+		const bitchify = new Bitchify();
+
+		expect(bitchify.render().active).toBeTruthy();
+
+		// TODO: return itself?
+		expect(bitchify.render()).toBeFalsy();
+	});
+
+	it('should render on initialize, bitch!', () => {
 		const bitchify = new Bitchify({
 			active: true
 		});
 
 		expect(bitchify.active).toBeTruthy();
-
-		// TODO: return itself?
-		expect(bitchify.render()).toBeFalsy();
 	});
+
+	// it('should render on hash change, bitch!', () => {
+	// 	const bitchify = new Bitchify();
+	//
+	// 	expect(bitchify.active).toBeFalsy();
+	// 	window.location.hash = 'bitch';
+	// 	expect(bitchify.active).toBeTruthy();
+	// });
 
 });
